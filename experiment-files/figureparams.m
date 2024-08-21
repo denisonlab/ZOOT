560b30f5bd89b4e238cabc4f126290612e07346a
jmotzer <jmotzer@bu.edu>
2024-08-21 15:28:05 -0400

    collected data for SDT variable plots
    continue when return

--- experiment-files/figureparams.m
+++ experiment-files/figureparams.m
@@ -1,8 +1,9 @@
 function fp = figureparams;
 
 %% colors 
-% fp.blue = [0 0.4470 0.7410];
-% fp.orange = [0.8500 0.3250 0.0980];
+ fp.blue = [0 0.4470 0.7410];
+ fp.lightblue = [  0.5843    0.8157    0.9882];
+ fp.orange = [0.8500 0.3250 0.0980];
 % fp.gold  = [0.9290 0.6940 0.1250];
 % fp.green = [0.4660 0.6740 0.1880];
 
