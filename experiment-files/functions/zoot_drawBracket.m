3cf24c2645d3c178bdc13085de96107b158411f1
Karen Tian <9136901+karentian@users.noreply.github.com>
2025-03-22 00:30:30 -0400

    add line width option to stat brackets

--- experiment-files/functions/zoot_drawBracket.m
+++ experiment-files/functions/zoot_drawBracket.m
@@ -1,6 +1,12 @@
-function zoot_drawBracket(x1,x2,y)
+function zoot_drawBracket(x1,x2,y,linewidth)
 % Draws bracket for indicating statistical test 
 
+%% Check inputs
+if nargin<4
+    linewidth = 1; % default
+end
+
+%% 
 yl = ylim;
 xl = xlim;
 
@@ -8,13 +14,8 @@ y = y + 0.05; % correction to better align with significance indicator
 bracketHeight = (yl(2)-yl(1))/45; % 0.007 
 bracketColor = 'black'; 
 
-lineWidth = 0.75; 
+% draw 4 point line 
+x = [x1; x1; x2; x2]; 
+y = [max(yl)*y-bracketHeight; max(yl)*y; max(yl)*y; max(yl)*y-bracketHeight]; 
 
-x = [x1 x1;... % vertical left 
-    x1 x2;... % horizontal 
-    x2 x2]; % vertical right 
-y = [max(yl)*y-bracketHeight max(yl)*y;...
-    max(yl)*y max(yl)*y;... 
-    max(yl)*y max(yl)*y-bracketHeight]; 
-
-plot(x,y,'LineWidth',lineWidth,'Color',bracketColor)
+plot(x,y,'LineWidth',linewidth,'Color',bracketColor,'LineJoin','miter')
