2eecf9dff94875d7f45b9e5b96089c1322da65b3
Karen Tian <9136901+karentian@users.noreply.github.com>
2025-03-22 00:25:30 -0400

    clean fig

--- experiment-files/analysos/Fig2a_TXAcc.m
+++ experiment-files/analysos/Fig2a_TXAcc.m
@@ -24,93 +24,71 @@ load(datafile)
 
 % Figure styling
 fp = zoot_figureparams;
-
-% /// Define your xVals once. Then, they can be used throughout.
-% /// Would be even better to define in figureparams once to be used across all figures. 
-% xVals = [0.7778 1 1.222;... % T1 (
-%     1.7778 2 2.2222]; % T2
 xVals = fp.xVals; 
-
-% /// Also define colors just once (see changes to figure params). Make use of logical indexing rather
-% than if loops.
-% /// Same comment for bar widths (see changes to figure params). 
-% /// size of figure should also be just defined once in figureparams. 
 
 %% FIGURE 2A: Acc by target contrast
 figure
-set(gcf,'Position',[100 100 500 160])
+set(gcf,'Position',[100 100 500 180])
 
-for iContrast = 1:2
-    subplot(1,2,iContrast)
-    zoot_figureStyle; hold on
-    for iTarget = 1:2
-        for iValid = 1:3
-            b = bar(xVals(iTarget,iValid), tcAcc.mean(iContrast, iValid, iTarget),...
-                'LineWidth',fp.bar.lineWidth(iContrast),'BarWidth',fp.bar.barWidth(iContrast),...
-                'EdgeColor',fp.colorsTargets(iTarget,iValid,:),'FaceColor',fp.bar.FaceColor(iContrast,iTarget,iValid,:)); 
-            
-            % scatter 
-            swarmchart( xVals(iTarget,iValid), squeeze(tcAcc_scatterplot.(targ_contrasts{iContrast})(iValid,:,iTarget)),'filled',...
-                'SizeData',fp.scatter.size,'XJitterWidth',100,'MarkerFaceColor',fp.colorsTargets(iTarget,iValid,:),'MarkerEdgeColor',fp.scatter.edgeColor,'MarkerFaceAlpha',fp.scatter.alpha(iValid) ); 
-            
-            errorbar(xcoords(iContrast, iValid, iTarget),tcAcc.mean(iContrast, iValid, iTarget),tcAcc.err(iContrast, iValid, iTarget), '.k', 'CapSize', fp.CapSize, 'LineWidth', fp.ErrorBarLineWidth)
-        end
-
-    end
-
-    ylabel('Accuracy (%)    ')
+tcl = tiledlayout(1,2,'TileSpacing','compact','OuterPosition',[0 0 1 0.85]);
+tcl.YLabel.String = 'Accuracy (%)';
+for iC = 1:2
+    nexttile
+    zoot_figureStyle
     ylim([30 115])
-    set(gca, 'ytick', 30:10:100)
+    yticks(30:10:100)
     xlim([0.5 2.5])
     xticks([xVals(1,:) xVals(2,:)])
-    set(gca, 'xticklabel', {'V', 'N','I','V','N','I'})
-    % ax = gca;
-    % ax.Position=ax.Position.*[1 1 1 0.85]; % makes more space above fig
-
-    if plotStats
-        if iContrast==1
-            % T1 V-I
-            kt_annotateStats(1,(115*0.82)+1.5,'***');
-            zoot_drawBracket(fp.xVals(1,1), fp.xVals(1,3), 0.82) 
-            
-            % T1 N-I
-            kt_annotateStats(1.1111,(115*0.86)+1.5,'**');
-            zoot_drawBracket(fp.xVals(1,2), fp.xVals(1,3), 0.86)
-            
-            % T2 V-I
-            kt_annotateStats(2,(115*0.80)+1.5,'**');
-            zoot_drawBracket(fp.xVals(2,1), fp.xVals(2,3), 0.80) 
+    xticklabels({'V','N','I','V','N','I'})
+    for iT = 1:2
+        for iV = 1:3
+            bar(xVals(iT,iV), tcAcc.mean(iC, iV, iT),...
+                'LineWidth',fp.bar.lineWidth(iC),'BarWidth',fp.bar.barWidth(iC),...
+                'EdgeColor',fp.colorsTargets(iT,iV,:),'FaceColor',fp.bar.FaceColor(iC,iT,iV,:)); 
            
-            % kt_annotateStats(2.111,89.5,'*');
-            % kt_drawBracket(2, 2.2222, .875)
-            % 
-            % kt_annotateStats(1,98,'________');
-            % kt_annotateStats(1,99,'*** Validity');
-            % 
-            % kt_annotateStats(2,98,'________');
-            % kt_annotateStats(2,99,'* Validity');
+            swarmchart( xVals(iT,iV), squeeze(tcAcc_scatterplot.(targ_contrasts{iC})(iV,:,iT)),'filled',...
+                'SizeData',fp.scatter.size,'XJitterWidth',100,'MarkerFaceColor',fp.colorsTargets(iT,iV,:),'MarkerEdgeColor',fp.scatter.edgeColor,'MarkerFaceAlpha',fp.scatter.alpha(iV) ); 
             
-        elseif iContrast == 2
-            % T1 V-I
-            kt_annotateStats(1,91,'*');
-            zoot_drawBracket(fp.xVals(1,1), fp.xVals(1,3), 0.9)
-        end
+            errorbar(xcoords(iC,iV,iT),tcAcc.mean(iC,iV,iT),tcAcc.err(iC,iV,iT), '.k', 'CapSize', fp.CapSize, 'LineWidth', fp.ErrorBarLineWidth)
+        end     
     end
 
+    if plotStats
+        xPairs = [1 3; 1 2; 2 3]; % invalid-valid, invalid-neutral, neutral-valid
+        yMax = 0.93; yDiff = 0.04;
+        yBracket = [yMax yMax-yDiff yMax-2*yDiff]; % pairwise brackets y position 
+        yMain = 115; % significace text y position
+        yMax = 107; yDiff = 5; 
+        yPairs = [yMax yMax-yDiff yMax-yDiff*2]; % y position for significant text 
+        
+        clear stats % structure with stats information
+        % stats.pairwise{1} = {'***','**',''; '**','',''}; 
+        stats.pairwise{1} = {'***','**','*'; '**','***','*'}; % target by pair
+        stats.main{1} = {'***','*'}; 
+        for iT = 1:2 % two targets
+            % pairwise comparisons
+            for iP = 1:3 
+                txt = twcf_annotateStats( (xVals(iT,xPairs(iP,2)) + xVals(iT,xPairs(iP,1)))/2, yPairs(iP), stats.pairwise{1}{iT,iP});
+                txt.FontSize = 14;
+                zoot_drawBracket(xVals(iT,xPairs(iP,1)),xVals(iT,xPairs(iP,2)),yBracket(iP),0.5)
+            end
+            % main effect
+            txt = twcf_annotateStats(xVals(iT,2),yMain,stats.pairwise{1}{iT});
+            txt.FontSize = 14;
+        end
+    end
 end
 
+figTitle = 'TX_Acc';
+if saveplots
+    export_fig(gcf,sprintf('%s/%s.%s', figDir, figTitle, figType), '-transparent','-p10')
+    % print(gcf, '-dpdf', '/Users/jennymotzer/Documents/GitHub/ZOOT/experiment-files/groupFigs/TX_Acc.pdf') % needed or else background turns black, do not erase
+end
+
+%% Side labels
 % [ax3, h3] = suplabel('Target Absent', 't', [0.08 0.08 1.3 .88]);
 % [ax4, h4] = suplabel('Target Present', 't', [0.08 0.08 .45 .88]);
 % [ax5, h5] = suplabel('T1', 'tt', [0.08 0.08 .27 0.8]);
 % [ax6, h6] = suplabel('T2', 'tt', [0.08 0.08 .6 0.8]);
 % [ax7, h7] = suplabel('T1', 'tt', [0.08 0.08 1.15 0.8]);
 % [ax8, h8] = suplabel('T2', 'tt', [0.08 0.08 1.47 0.8]);
-
-figTitle = 'TX_Acc';
-if saveplots
-    export_fig(gcf,sprintf('%s/%s.%s', figDir, figTitle, figType), '-transparent','-p10')
-    % print(gcf, '-dpdf', '/Users/jennymotzer/Documents/GitHub/ZOOT/experiment-files/groupFigs/TX_Acc.pdf') % needed or else background turns black, do not erase
-    % /// The second print will override the first statement.
-    % so then, can just stick to one, either first or second. The second is
-    % not fixing the first, it's overriding the first. 
-end
