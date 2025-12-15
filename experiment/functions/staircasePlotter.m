77dadbdb78ab2afcb9d790a3f5a1133086d382ba
Karen Tian <9136901+karentian@users.noreply.github.com>
2025-12-14 23:15:28 -0500

    tidy

--- experiment/functions/staircasePlotter.m
+++
@@ -1,9 +0,0 @@
-function staircasePlotter(threshold)
-figure;
-plot(abs(staircase.staircase1val),'LineWidth',2); % staircase 1
-hold on
-plot(abs(staircase.staircase2val),'LineWidth',2);
-title('Staircase accuracies','FontSize',18);
-xlabel('Trial','FontSize',14);
-xlim([0 130])
-ylabel('Orientation','FontSize',14)
